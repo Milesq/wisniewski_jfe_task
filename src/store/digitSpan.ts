3a8323f3f45ae4ca41e7b1db838d07f2fe60068f
Milesq <wmilosz88@gmail.com>
2021-05-25 22:34:05 +0200

    feat: add createSequence reducer

--- src/store/digitSpan.ts
+++ src/store/digitSpan.ts
@@ -1,14 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import { random } from '../utils'
+
+import { RootState } from '.'
 
 export interface DigitSpanState {
   currentLevel: number
   score: number
+  currentSequence: number[]
 }
 
 const initialState: DigitSpanState = {
   currentLevel: 3,
   score: 0,
+  currentSequence: [],
 }
+
+const createSequence = createAsyncThunk(
+  'digitSpan/createSequence',
+  (_, { getState }) => {
+    const { digitSpan } = getState() as RootState
+
+    const newSequence = new Array(digitSpan.currentLevel)
+      .fill(0)
+      .map(() => random(0, 10))
+
+    return newSequence
+  }
+)
 
 export const slice = createSlice({
   name: 'digitSpan',
@@ -22,7 +41,14 @@ export const slice = createSlice({
       state.currentLevel--
     },
   },
+  extraReducers: builder => {
+    builder.addCase(createSequence.fulfilled, (state, { payload }) => {
+      state.currentSequence = payload
+    })
+  },
 })
+
+export { createSequence }
 
 export const {
   actions: { acceptAnswer, dismissAnswer },
