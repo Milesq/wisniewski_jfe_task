80024cb194bbc677609182d2ea50e4e777f598f9
Milesq <wmilosz88@gmail.com>
2021-05-26 12:37:27 +0200

    feat: start next level when click button

--- src/components/GameSummary.tsx
+++ src/components/GameSummary.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 import { useSelector } from '../store'
+import { createSequence } from '../store/digitSpan'
 
 import Badge from './Badge'
 
 function GameSummary() {
+  const dispatch = useDispatch()
   const { currentLevel, score } = useSelector(({ digitSpan }) => digitSpan)
+
+  function startNextLevel() {
+    dispatch(createSequence())
+  }
 
   return (
     <div className="bg-purple-400 p-8 flex flex-col gap-5">
       <Badge value={currentLevel}>Level</Badge>
       <Badge value={score}>Score</Badge>
       <button
+        onClick={startNextLevel}
         className="
           bg-green-400
           hover:bg-green-500
